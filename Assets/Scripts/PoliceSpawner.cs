0febb22ed6536aed20f13cea61be9f13fe3a5bba
santosh161616 <santosh161616@gmail.com>
2023-05-12 15:46:17 +0530

    Trying out different combinations

--- Assets/Scripts/PoliceSpawner.cs
+++ Assets/Scripts/PoliceSpawner.cs
@@ -10,28 +10,34 @@ public class PoliceSpawner : MonoBehaviour
     public float radius = 2f;    
     public GameObject prefab;
     public Vector2 center;
+    Vector2[] totalPositions; 
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < numberOfPolice; i++)
-        {
-            Vector2 position = GetRandomPositionOutsideCircle();
-            Instantiate(prefab, position, Quaternion.identity);
-        }
-        
+        totalPositions = new Vector2[numberOfPolice];
+        TotoalPositions();                             
     }
 
-    Vector2 GetRandomPositionOutsideCircle()
+    public void TotoalPositions()
     {
+        for (int i = 0; i < numberOfPolice; i++)
+        {                        
+            totalPositions[i] = GetRandomPositionOutsideCircle();
+            Instantiate(prefab, totalPositions[i], Quaternion.identity);
+        }        
+    }
+
+    Vector2 GetRandomPositionOutsideCircle() {
         Vector2 position = new Vector2(Random.Range(center.x - radius - 1f, center.x + radius + 1f),
-                                       Random.Range(center.y - radius - 1f, center.y + radius + 1f));
+                                   Random.Range(center.y - radius - 1f, center.y + radius + 1f));
         while (Vector2.Distance(position, center) <= radius)
         {
             position = new Vector2(Random.Range(center.x - radius - 1f, center.x + radius + 1f),
                                    Random.Range(center.y - radius - 1f, center.y + radius + 1f));
         }
         return position;
+    
     }
 }
 
